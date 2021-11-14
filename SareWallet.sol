c2285755c01cc24eccfa70c80d8deb5c14ce7ce3
code salley <juxsalley@gmail.com>
2021-11-14 18:25:06 +0000

    wallet permission

--- SareWallet.sol
+++ SareWallet.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract Permission {
     address owner;
 
@@ -16,5 +18,12 @@ contract Permission {
 }
 
 contract ShareWallet is Permission {
+    function withrawMoney(address payable _to, uint256 _amount)
+        public
+        OnlyOwner
+    {
+        _to.transfer(_amount);
+    }
+
     receive() external payable {}
 }
