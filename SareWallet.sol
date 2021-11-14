0de359974b10315e9c2771db856e43ffcd6c8721
code salley <juxsalley@gmail.com>
2021-11-14 23:24:53 +0000

    add reduce allowance implementation

--- SareWallet.sol
+++ SareWallet.sol
@@ -5,9 +5,24 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ShareWallet is Ownable {
+    mapping(address => uint) public allowance;
+    
+    modifier ownerOrAllow(uint _amount) {
+        require(owner() == msg.sender || allowance[msg.sender] >= _amount);
+        _;
+    }
+    
+    function reduceAllowance (address _who, uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
+    
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
     function withrawMoney(address payable _to, uint256 _amount)
         public
-        onlyOwner
+        ownerOrAllow(_amount)
+        
     {
         _to.transfer(_amount);
     }
